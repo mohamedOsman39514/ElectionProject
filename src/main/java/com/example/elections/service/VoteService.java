ab55682f1750bf85b3c18099ff75f988d725da30
Mohamed Osman <74077714+mohamedOsman39514@users.noreply.github.com>
2022-09-04 10:59:16 +0200

    commit

--- src/main/java/com/example/elections/service/VoteService.java
+++ src/main/java/com/example/elections/service/VoteService.java
@@ -1,9 +1,37 @@
 package com.example.elections.service;
 
+import com.example.elections.model.Vote;
+import com.example.elections.model.Voter;
+import com.example.elections.repository.VoteRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class VoteService {
+
+
+    @Autowired
+    private VoteRepository voteRepository;
+
+    public List<Vote> getAllVotes() {
+        return voteRepository.findAll();
+    }
+
+    public Optional<Vote> getVote(Long id) {
+        return voteRepository.findById(id);
+    }
+
+    public void delete(Long id) {
+        voteRepository.deleteById(id);
+    }
+
+    public Vote save(Vote vote) {
+        return voteRepository.save(vote);
+    }
+
 }
